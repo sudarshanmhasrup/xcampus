ebd5ed847c4bfa3c3d1ab864a6c3f6dbc0712a61
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-08-07 20:39:01 +0530

    Configure dotenv package.

--- api/app.js
+++ api/app.js
@@ -1,6 +1,17 @@
 // Import the neccessary packages
 const express = require("express");
+
+// Configure dotenv
+require("dotenv").config();
+
+// Configure the express app
 const app = express();
+
+app.get("/test", (request, response) => {
+    response.status(200).json({
+        
+    });
+});
 
 // Export the express app
 module.exports = app;
